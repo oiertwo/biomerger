cf9e71fea2f3bc6606354d0b8657b804d478909b
oier <oier.etxaniz@gmail.com>
2016-05-30 20:11:01 +0200

    works for 2 and 3 files.

--- biomerger/merger.py
+++ biomerger/merger.py
@@ -33,3 +33,45 @@ def merge(files, f_out, compress=True):
         with open(f_out, 'rb') as f_in, gzip.open("{}.gz".format(f_out), 'wb') as f_out:
             shutil.copyfileobj(f_in, f_out)
 
+
+
+def merge2(f1, f2, f_out, compress=True):
+
+    outfile = open(f_out, "w")
+    iter1 = SeqIO.parse(open(f1), "fastq")
+    iter2 = SeqIO.parse(open(f2), "fastq")
+
+    for t in zip_longest(iter1, iter2):
+        add = []
+        for rec in t:
+            if (rec != None):
+                add.append(rec)
+        SeqIO.write(add, outfile, "fastq")
+    outfile.close()
+
+    # compress
+    if compress:
+        with open(f_out, 'rb') as f_in, gzip.open("{}.gz".format(f_out), 'wb') as f_out:
+            shutil.copyfileobj(f_in, f_out)
+
+
+def merge3(f1, f2, f3, f_out, compress=True):
+
+    outfile = open(f_out, "w")
+    iter1 = SeqIO.parse(open(f1), "fastq")
+    iter2 = SeqIO.parse(open(f2), "fastq")
+    iter3 = SeqIO.parse(open(f3), "fastq")
+
+
+    for t in zip_longest(iter1, iter2, iter3):
+        add = []
+        for rec in t:
+            if (rec != None):
+                add.append(rec)
+        SeqIO.write(add, outfile, "fastq")
+    outfile.close()
+
+    # compress
+    if compress:
+        with open(f_out, 'rb') as f_in, gzip.open("{}.gz".format(f_out), 'wb') as f_out:
+            shutil.copyfileobj(f_in, f_out)
